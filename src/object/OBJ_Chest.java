8115304e88da17abb03cea2be27d55ddf7b833cc
melanierefman <87106838+melanierefman@users.noreply.github.com>
2022-12-07 16:25:53 +0700

    add hit monster (not perfect)

---
+++ src/object/OBJ_Chest.java
@@ -0,0 +1,18 @@
+package object;
+
+import entity.Entity;
+import main.GamePanel;
+
+public class OBJ_Chest extends Entity{
+//	public class OBJ_Chest extends SuperObject{
+
+	public OBJ_Chest(GamePanel gp) {
+		
+		super(gp);
+		
+		name = "Chest";
+		down1 = setup("/objects/chest", gp.tileSize, gp.tileSize);
+//		down1 = setup("/object/chest", gp.tileSize, gp.tileSize);
+
+	}
+}
